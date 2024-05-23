b82d628bcf4904128a5c0eeba938983be8f06b9b
nadiavp <nadia.panossian@nrel.gov>
2024-05-23 13:53:04 -0600

    sites residential and commercial storage

--- source/custom_controls/btms_siting.py
+++ source/custom_controls/btms_siting.py
@@ -2,32 +2,37 @@
 ### and develops a dictionary of distribution buses with btm storage
 ### that dictionary includes storage size, 
 ### max charging and discharging power, and min charging and discharing power
+### all values here are in kW or kva
 import pandas as pd
 from opendssdirect import dss
 import sys
 
-def get_load_points(dss_loads=[], load_threshold=350, pv_threshold=5):
+def get_load_points(dss_loads=[], load_threshold=25, pv_threshold=5):
     # only add the load point if the threshold is above 350kw
     # or pv capacity is above 5 kw
     bus_df = pd.DataFrame({'bus_name':[],'load_peak':[], 'pv_cap':[]})
-    for bus_load in dss_loads:
+    for load_name in dss_loads:
         # if the load is negative, assume pv generation
         # if positive assume normal load
-        kva_base = bus_load.kVABase()
-        loadshape_name = bus_load.Yearly() # try .Daily() if only 24 hour sim
-        loadshape = dss.Loadshape.Name(loadshape_name)
-        peak = max(loadshape.PMult())*kva_base
-        bus_name = bus_load.bus()[0]
-        if -peak > pv_threshold*1000:
+        dss.Loads.Name(load_name)
+        kva_base = dss.Loads.kVABase()
+        loadshape_name = dss.Loads.Yearly() # try .Daily() if only 24 hour sim
+        if not loadshape_name=='':
+            loadshape = dss.LoadShape.Name(loadshape_name)
+            peak = max(dss.LoadShape.PMult())*kva_base
+        else:
+            peak = dss.Loads.kW()
+        bus_name = dss.CktElement.BusNames()[0]
+        if -peak > pv_threshold:
             # first check to see if it's been added yet
             if not (bus_name in bus_df['bus_name']):
                 bus_df._append({'bus_name':bus_name,'load_peak':0, 'pv_cap':0})
-            bus_df[bus_df['bus_name']==bus_name]['pv_cap'] += peak
-        elif peak > load_threshold*1000:
+            bus_df.loc[bus_df['bus_name']==bus_name,'pv_cap'] = bus_df.loc[bus_df['bus_name']==bus_name,'pv_cap'] + peak
+        elif peak > load_threshold:
             # first check to see if it's been added yet
             if not (bus_name in bus_df['bus_name']):
-                bus_df._append({'bus_name':bus_name,'load_peak':0, 'pv_cap':0})
-            bus_df[bus_df['bus_name']==bus_name]['load_peak'] += peak
+                bus_df = bus_df._append({'bus_name':bus_name,'load_peak':0, 'pv_cap':0}, ignore_index=True)
+            bus_df.loc[bus_df['bus_name']==bus_name,'load_peak'] = bus_df.loc[bus_df['bus_name']==bus_name,'load_peak'] + peak
     return bus_df
 
 
@@ -43,15 +48,14 @@ def assign_battery_sizes(bus_df):
     # dataframe of storage by bus
     bess_df = {'bus_name':[], 'storage_cap_kwh':[], 'storage_power_kw':[]}
     for _, bus in bus_df.iterrows():
-        breakpoint()
         bess_df['bus_name'].append(bus)
-        if bus['pv_cap'] > 25000 or bus['load_peak'] > 1e6:
+        if bus['pv_cap'] > 25 or bus['load_peak'] > 1000: # in kW
             cap = lgc_bes['storage_cap_kwh']
             pwr = lgc_bes['storage_power_kw']
-        elif bus['pv_cap'] > 12000 or bus['load_peak'] > 350000:
+        elif bus['pv_cap'] > 12 or bus['load_peak'] > 350:
             cap = smc_bes['storage_cap_kwh']
             pwr = smc_bes['storage_power_kw']
-        elif bus['pv_cap'] > 7000 or bus['load_peak'] > 15000:
+        elif bus['pv_cap'] > 7 or bus['load_peak'] > 15:
             cap = lgr_bes['storage_cap_kwh']
             pwr = smc_bes['storage_power_kw']
         else:
@@ -67,9 +71,9 @@ if __name__ == "__main__":
     # first load the loadshape file
     if len(sys.argv)>1:
         opendss_file = sys.argv[1]
-        dss_model = dss.Command(f'Redirect {opendss_file}')
-        dss.Solve()
-        dss_loads = dss.Loads
+        dss_model = dss.run_command(f'Redirect {opendss_file}')
+        dss.Solution.Solve()
+        dss_loads = dss.Loads.AllNames()
     else:
         dss_loads = []
     bus_df = get_load_points(dss_loads)
